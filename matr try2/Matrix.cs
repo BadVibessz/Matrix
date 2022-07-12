6ea5dec272ccc1f9216c7295098c6e3831223cb2
BadVibessz <danila.tsymbal@mail.ru>
2022-07-12 16:31:11 +0300

    Added ToTriangular() that uses Gaussian method Added function to calculate unknowns by Gaussian elimination

--- matr try2/Matrix.cs
+++ matr try2/Matrix.cs
@@ -59,11 +59,34 @@ namespace matr_try2
             set { }
             get { return this.values.Length / values.GetLength(0); }
         }
-        
+
         public double this[int i, int j]
         {
             get => this.values[i, j];
         }
+
+
+        public int Rang
+        {
+            get
+            {
+                var copy = new Matrix(this);
+                copy.ToTriangularMatrix();
+
+                int count = 0;
+                for (int i = 0; i < copy.Row; i++)
+                {
+                    int temp = 0;
+                    for (int j = 0; j < copy.Col; j++)
+                        if (copy[i, j] != 0)
+                            temp++;
+                    if (temp != 0) count++;
+                }
+
+                return count;
+            }
+        }
+
 
         public override bool Equals(object obj)
         {
@@ -84,27 +107,6 @@ namespace matr_try2
         {
             return values.GetHashCode();
         }
-
-        // public override string ToString()
-        // {
-        //     List<char> temp = new List<char>();
-        //
-        //     // for (int i = 0; i < this.Row; i++)
-        //     // {
-        //     //     for (int j = 0; j < this.Col; j++)
-        //     //         temp.Add(Convert.ToChar(this.values[i, j]));
-        //     //     temp.Add(' ');
-        //     // }
-        //
-        //     for (int i = 0; i < this.Row; i++)
-        //     {
-        //         for (int j = 0; j < this.Col; j++)
-        //             if(Boolean.TryParse(this[i,j], char))
-        //             temp.Add(Convert.ToChar(this[i, j]));
-        //         temp.Add(' ');
-        //     }
-        //     return temp.ToString();
-        // }
 
         public static bool operator ==(Matrix m1, Matrix m2)
         {
@@ -235,6 +237,93 @@ namespace matr_try2
                 ar[i, j] = m.values[j, i];
             return new Matrix(ar);
         }
+
+
+        private void SwapRows(int i, int j)
+        {
+            for (int k = 0; k < Col; k++)
+                (values[i, k], values[j, k]) = (values[j, k], values[i, k]);
+        }
+
+        public void ToTriangularMatrix()
+        {
+            for (int k = 0; k < Row; k++)
+            {
+                // Initialize maximum value and index for pivot
+                int i_max = k;
+                int v_max = (int)values[i_max, k];
+
+                /* find greater amplitude for pivot if any */
+                for (int i = k + 1; i < Row; i++)
+                {
+                    if (Math.Abs(values[i, k]) > v_max)
+                    {
+                        v_max = (int)values[i, k];
+                        i_max = i;
+                    }
+
+                    /* If a principal diagonal element  is zero,
+                    *  it denotes that matrix is singular, and
+                    *  will lead to a division-by-zero later. */
+                    if (values[k, i_max] == 0)
+                        throw new Exception("Matrix is singular"); // Matrix is singular
+
+                    /* Swap the greatest value row with
+                       current row
+                    */
+                    if (i_max != k)
+                        SwapRows(k, i_max);
+
+                    for (int l = k + 1; l < Row; l++)
+                    {
+                        /* factor f to set current row kth element
+                        *  to 0, and subsequently remaining kth
+                        *  column to 0 */
+                        double f = values[l, k] / values[k, k];
+
+                        /* subtract fth multiple of corresponding
+                           kth row element*/
+                        for (int j = k + 1; j < Col; j++)
+                            values[l, j] -= values[k, j] * f;
+
+                        /* filling lower triangular matrix with
+                        *  zeros*/
+                        values[l, k] = 0;
+                    }
+                }
+            }
+        }
+
+
+        public double[] CalculateUnknowns()
+        {
+            // An array to store solution
+            double[] x = new double[Col];
+
+            /* Start calculating from last equation up to the
+               first */
+            for (int i = Row - 1; i >= 0; i--)
+            {
+                /* start with the RHS of the equation */
+                x[i] = values[i, Row];
+
+                /* Initialize j to i+1 since matrix is upper
+                triangular*/
+                for (int j = i + 1; j < Row; j++)
+                {
+                    /* subtract all the lhs values
+                     * except the coefficient of the variable
+                     * whose value is being calculated */
+                    x[i] -= values[i, j] * x[j];
+                }
+
+                /* divide the RHS by the coefficient of the
+                unknown being calculated */
+                x[i] /= values[i, i];
+            }
+
+            return x;
+        }
     }
 
     class SquareMatrix : Matrix
@@ -254,9 +343,6 @@ namespace matr_try2
         {
         }
 
-        // public SquareMatrix(InvertibleMatrix other) : base(other)
-        // {
-        // }
 
         public SquareMatrix(Matrix other) : base(other)
         {
@@ -265,25 +351,26 @@ namespace matr_try2
 
 
         public static SquareMatrix operator +(SquareMatrix a, SquareMatrix b) =>
-            new SquareMatrix((Matrix) a + b);
+            new SquareMatrix((Matrix)a + b);
 
         public static SquareMatrix operator +(SquareMatrix a, double n) =>
-            new SquareMatrix((Matrix) a + n);
+            new SquareMatrix((Matrix)a + n);
 
         public static SquareMatrix operator -(SquareMatrix a, SquareMatrix b) =>
-            new SquareMatrix((Matrix) a - b);
+            new SquareMatrix((Matrix)a - b);
 
         public static SquareMatrix operator -(SquareMatrix a, double n) =>
-            new SquareMatrix((Matrix) a - n);
+            new SquareMatrix((Matrix)a - n);
 
         public static SquareMatrix operator *(SquareMatrix a, SquareMatrix b) =>
-            new SquareMatrix((Matrix) a * b);
+            new SquareMatrix((Matrix)a * b);
 
         public static SquareMatrix operator *(SquareMatrix a, double n) =>
-            new SquareMatrix((Matrix) a * n);
+            new SquareMatrix((Matrix)a * n);
+
 
         public static SquareMatrix operator ~(SquareMatrix a) =>
-            new SquareMatrix((Matrix) (~a));
+            new SquareMatrix((Matrix)(~a));
 
 
         //maybe private
@@ -393,22 +480,22 @@ namespace matr_try2
 
 
         public static InvertibleMatrix operator +(InvertibleMatrix a, InvertibleMatrix b) =>
-            new InvertibleMatrix((SquareMatrix) a + b);
+            new InvertibleMatrix((SquareMatrix)a + b);
 
         public static InvertibleMatrix operator +(InvertibleMatrix a, double n) =>
-            new InvertibleMatrix((SquareMatrix) a + n);
+            new InvertibleMatrix((SquareMatrix)a + n);
 
         public static InvertibleMatrix operator -(InvertibleMatrix a, InvertibleMatrix b) =>
-            new InvertibleMatrix((SquareMatrix) a - b);
+            new InvertibleMatrix((SquareMatrix)a - b);
 
         public static InvertibleMatrix operator -(InvertibleMatrix a, double n) =>
-            new InvertibleMatrix((SquareMatrix) a - n);
+            new InvertibleMatrix((SquareMatrix)a - n);
 
         public static InvertibleMatrix operator *(InvertibleMatrix a, InvertibleMatrix b) =>
-            new InvertibleMatrix((SquareMatrix) a * b);
+            new InvertibleMatrix((SquareMatrix)a * b);
 
         public static InvertibleMatrix operator *(InvertibleMatrix a, double n) =>
-            new InvertibleMatrix((SquareMatrix) a * n);
+            new InvertibleMatrix((SquareMatrix)a * n);
 
         public InvertibleMatrix Invert()
         {
--- matr try2/Program.cs
+++ matr try2/Program.cs
@@ -4,30 +4,11 @@ namespace matr_try2
 {
     class Program
     {
+        
+        
         static void Main(string[] args)
         {
-            var x = new Matrix(new double[3, 3] {{1, 0, 4}, {0, -1, -2}, {0, 0, 0}});
-            var y = new SparseMatrix(x);
 
-            for (int i = 0; i < x.Row; i++)
-            {
-                for (int j = 0; j < x.Col; j++)
-                {
-                    Console.Write(x[i, j]);
-                    Console.Write(" ");
-                }
-            
-                Console.WriteLine();
-            }
-            
-
-            Console.WriteLine();
-            var test = y.Neighbours(y[-100, -1]);
-            foreach (var i in test)
-            {
-                Console.Write(i.value);
-                Console.Write(" ");
-            }
         }
     }
 }
